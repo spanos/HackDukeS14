9f76f5b437a21e768325fa268dd5743a13c886d0
Nicole <nicole@effie-ThinkPad-T410.(none)>
2014-03-29 18:59:18 -0400

    first version of the input page

---
+++ home.php
@@ -0,0 +1,22 @@
+<body>
+
+<h1>Add New Event</h1>
+
+<form name="event" action="event.js" method="get">
+Title: <input type="text" name="title"><br>
+Date: <input type="text" name="date"><br>
+Time: <input type="text" name="time"><br>
+Place: <input type="text" name="place"><br>
+<input type="submit" value="Add New Event">
+</form>
+
+<h1>Add New Resident</h1>
+
+<form name="resident" action="resident.js" method="get">
+Name: <input type="text" name="name"><br>
+Phone: <input type="text" name="phone"><br>
+<input type ="submit" value"Add New Resident">
+</form>
+
+</body>
+</html>
