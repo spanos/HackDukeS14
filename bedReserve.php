9c73657f423e5e3d5419f1ffc950d8578e9273ba
David Spanos <david.spanos404@gmail.com>
2014-03-30 12:23:55 -0400

    Create bedReserve.php

---
+++ bedReserve.php
@@ -0,0 +1,29 @@
+<?php
+
+//Redirect to home page after action
+header( 'Location: http://valakuzhy.dx.am/home.php' ) ;
+
+// Create connection
+$con = mysqli_connect("fdb6.awardspace.net","1652346_db1","hackduke14","1652346_db1");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+$BedID = $_REQUEST['BedID'];
+  
+// Insert into db
+$sql = "UPDATE Beds 
+        SET IsOpen=1
+        WHERE BedId='$BedID'" ;
+
+if (!mysqli_query($con, $sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+echo "1 bed reserved";
+
+// Close
+mysqli_close($con);
+?> 
